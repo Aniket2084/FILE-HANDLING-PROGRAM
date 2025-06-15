5809aba72b0baeddcfc8831a508918847487dd99
Aniket2084 <aniketgolu2084@gmail.com>
2025-06-15 11:57:40 +0530

    Update file.c

--- file.c
+++ file.c
@@ -1,29 +1,3 @@
-#include <stdio.h>
 
-int main() {
-    FILE *fptr;
-    //writting in flie
-    fptr=fopen("test.txt","w");
-    char str[100];
-    printf("Enter the string to be written=");
-    scanf("%s",&str);
-    fprintf(fptr,"%s",str);
-    fclose(fptr);
-    
-    //appending in FILE
-    fptr=fopen("test.txt","a");
-    char append[100];
-    printf("Enter the string to be appended=");
-    scanf("%s",&append);
-    fprintf(fptr,"%s",append);
-    fclose(fptr);
-    
-    //reading from FILE
-    fptr=fopen("test.txt","r");
-    char read[200];
-    fscanf(fptr,"%s",&read);
-    puts(read);
-    fclose(fptr);
-    
-    return 0;
-}
+
+
